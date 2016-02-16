615a6bcfc988b85778e1acfbf2b655cb8ac57647
Fran Iglesias <franiglesias@mac.com>
2016-02-16 23:17:19 +0100

    Extracting classes

--- src/AppBundle/Command/SendCommand.php
+++ src/AppBundle/Command/SendCommand.php
@@ -9,35 +9,110 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-
 use Symfony\Component\Finder\Finder;
 
+/**
+ * Represents a Payroll file
+ *
+ * @package default
+ * @author Francisco Iglesias Gómez
+ */
 class Payroll {
 	private $file;
-	private $email;
+	private $id;
 	private $name;
 	private $path;
+	private $email;
 	
 	public function __construct($file)
 	{
 		$this->file = $file;
+		$this->name = $this->extractNameFromFileName();
+		$this->id = $this->extractIdFromFileName();
+		$this->path = $this->file->getRealpath();
 	}
 	
-	public function getEmail()
+	public function getId()
 	{
-		# code...
+		return $this->id;
 	}
 	
 	public function getName()
 	{
-		# code...
+		return $this->name;
 	}
 	
 	public function getFile()
 	{
-		# code...
+		return $this->path;
+	}
+	
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+	
+	public function getEmail()
+	{
+		return $this->email;
+	}
+	
+	public function getTo()
+	{
+		return array($this->email => $this->name);
+	}
+	
+	private function extractNameFromFileName()
+	{
+		$filename = $this->file->getRelativePathname();
+		preg_match('/nombre_\((.*), (.*)\)/', $filename, $matches);
+		return mb_convert_case($matches[2].' '.$matches[1], MB_CASE_TITLE);
+	}
+	
+	private function extractIdFromFileName()
+	{
+		$filename = $this->file->getRelativePathname();
+		preg_match('/trabajador_(\d+_\d+)/',$filename, $matches);
+		return $matches[1];
 	}
 }
+
+
+/**
+ * Repository for the email list
+ *
+ * @package default
+ * @author Francisco Iglesias Gómez
+ */
+class PayrollRepository {
+	private $emails;
+	
+	public function __construct($path)
+	{
+		$this->emails = $this->load($path.'/emails.dat');
+	}
+	
+	public function get($file)
+	{
+		$Payroll = new Payroll($file);
+		$Payroll->setEmail($this->emails[$Payroll->getId()]);
+		return $Payroll;
+	}
+	
+	private function load($path)
+	{
+		$emails = array();
+		foreach (file($path) as $line) {
+			list($id, $email) = explode(chr(9), $line);
+			$emails[$id] = trim($email);
+		}
+		return $emails;
+	}
+	
+}
+
+
+
 
 class SendCommand extends ContainerAwareCommand
 {
@@ -62,19 +137,24 @@ class SendCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 		$dataPath = __DIR__.'/../../../payroll';
-		$this->emails = $this->readEmails($dataPath.'/emails.dat', $output);
+
 		$this->month = $input->getArgument('month');
-		$finder = new Finder();
 		
+		$finder = new Finder();
 		$finder->files()->in($dataPath.'/'.$this->month);
+		
+		$repo = new PayrollRepository($dataPath);
+		
 		$count = 0;
 		foreach ($finder as $file) {
-			$email = $this->getEmailForFile($file);
-			$name = $this->getNameForFile($file);
-			$output->writeln('['.$count.'] Sending email to: '.$name);
-			if (!$this->sendEmail(array($email => $name), $file->getRealpath())) {
-				$output->writeln('Problem with email: '.$email);
+			$payroll = $repo->get($file);
+
+			$output->writeln('['.$count.'] Sending email to: '.$payroll->getName());
+
+			if (!$this->sendEmail($payroll)) {
+				$output->writeln('Problem with email: '.$payroll->getEmail());
 			}
+
 			$count = $count + 1;;
 			if ($count > 1) {
 				break;
@@ -120,8 +200,9 @@ class SendCommand extends ContainerAwareCommand
 		return $this->extractNameFromFileName($file->getRelativePathname()); 
 	}
 	
-	private function sendEmail($to, $file)
+	private function sendEmail($payroll)
 	{
+		
 		$sender = array('administracion@miralba.org' => 'Administración Colegio Miralba');
 		// Create the message
 		$message = \Swift_Message::newInstance()
@@ -135,7 +216,7 @@ class SendCommand extends ContainerAwareCommand
 			->setReplyTo('administracion@miralba.org')
 
 			// Set the To addresses with an associative array
-			->setTo($to)
+			->setTo($payroll->getTo())
 
 			// Give it a body
 			->setBody(sprintf('Estimado/a %s:'.chr(10).chr(10).'Adjuntamos tu nómina del mes de %s', current($to), $this->month))
@@ -144,7 +225,7 @@ class SendCommand extends ContainerAwareCommand
 			->addPart(sprintf('<p>Estimado/a %s:</p><p>Adjuntamos tu nómina del mes de %s</p>', current($to), $this->month), 'text/html')
 
 			// Optionally add any attachments
-			->attach(\Swift_Attachment::fromPath($file));
+			->attach(\Swift_Attachment::fromPath($payroll->getFile()));
 			
 		return $this->getContainer()->get('mailer')->send($message);
 	}

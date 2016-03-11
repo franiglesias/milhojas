d9351530a8d4581cd55227d21fddd879c47fa750
franiglesias <franiglesias@mac.com>
2016-03-11 15:30:37 +0100

    Removing SupplyLevel class to avoid clutter

--- src/Infrastructure/Network/Printers/DSM745PrinterDriver.php
+++ src/Infrastructure/Network/Printers/DSM745PrinterDriver.php
@@ -3,9 +3,9 @@
 namespace Milhojas\Infrastructure\Network\Printers;
 
 use Milhojas\Infrastructure\Network\Printers\PrinterDriverInterface;
-use Milhojas\Infrastructure\Network\Printers\DSM745SupplyLevel;
 use Milhojas\Library\ValueObjects\Technical\SupplyLevel;
 use Milhojas\Library\ValueObjects\Technical\Vendor;
+
 /**
 * Printer Adapter for Ricoh DSM-745
 */
@@ -36,8 +36,33 @@ class DSM745PrinterDriver implements PrinterDriverInterface
 	{
 		return new Vendor(static::VENDOR, static::MODEL);
 	}
+}
+
+class DSM745SupplyLevel extends SupplyLevel {
 	
+	private $levels = array(
+		'01' => 5,
+		'02' => 4,
+		'03' => 3,
+		'04' => 2,
+		'05' => 1,
+		'06' => 0
+	);
+	
+	public function __construct($level)
+	{
+		$this->isValidLevel($level);
+		parent::__construct($this->levels[$level]);
+	}
+	
+	private function isValidLevel($level)
+	{
+		if (! isset($this->levels[$level])) {
+			throw new \InvalidArgumentException(sprintf("Invalid level format %s provided by DSM745 Adapter", $level), 1);
+		}
+	}
 	
 }
+
 
 ?>

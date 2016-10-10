ca558c09102fcdbba911b8ada021e0c4a484befc
franiglesias <franiglesias@mac.com>
2016-10-10 17:11:48 +0200

    Creating a Milhojas User class to manage Authentication

---
+++ src/Milhojas/UsersBundle/UserProvider/MilhojasUser.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Milhojas\UsersBundle\UserProvider;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class MilhojasUser implements UserInterface
+{
+    /**
+     * @var string
+     */
+    protected $username;
+	
+	/**
+	 * stores a list of roles assigned to this user
+	 *
+	 * @var string
+	 */
+	protected $roles;
+
+    /**
+     * @param string $username
+     */
+    public function __construct($username)
+    {
+        $this->username = $username;
+		$this->roles = ['ROLE_USER'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPassword()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function eraseCredentials()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function equals(UserInterface $user)
+    {
+        return $user->getUsername() === $this->username;
+    }
+	
+	public function assignNewRole($role)
+	{
+		$this->roles = array_unique(array_merge($this->roles, (array) $role));
+	}
+}
+
+?>

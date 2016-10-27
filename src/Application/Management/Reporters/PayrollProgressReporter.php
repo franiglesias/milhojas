87bbe8afe21b16d99812380ccae29916099ec3b6
Fran Iglesias <franiglesias@mac.com>
2016-10-27 19:46:17 +0200

    Coding style fixes

--- src/Application/Management/Reporters/PayrollProgressReporter.php
+++ src/Application/Management/Reporters/PayrollProgressReporter.php
@@ -5,13 +5,13 @@ namespace Milhojas\Application\Management\Reporters;
 use Milhojas\Library\EventBus\EventHandler;
 use Milhojas\Library\EventBus\Event;
 /**
-* Writes Payroll Distribution Progress to a json file
+* Writes Payroll Distribution Progress to the json file specified in the constructor
 */
 class PayrollProgressReporter implements EventHandler
 {
 	private $file;
 	
-	function __construct($file)
+	public function __construct($file)
 	{
 		$this->file = $file;
 	}

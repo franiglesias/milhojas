232d4d259696637958690740606320dfeb02ce03
Fran Iglesias <franiglesias@mac.com>
2016-11-13 22:13:39 +0100

    CourseBuilderTest

---
+++ tests/Domain/School/CourseBuilderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Domain\School;
+use Milhojas\Domain\School\Course;
+
+/**
+ * Test CourseBuilder class
+ */
+class CourseBuilderClass extends \PHPUnit_Framework_Testcase
+{
+
+  public function setUp()
+  {
+    # code...
+  }
+
+  public function test_it_can_create_a_course()
+  {
+    $course = new Course();
+  }
+}
+
+ ?>

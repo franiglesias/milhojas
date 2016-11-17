e61d2dfe5fc3f138d65f49dd71d0456afdf01279
franiglesias <franiglesias@mac.com>
2016-11-17 12:46:11 +0100

    New version of Checklist

---
+++ spec/Milhojas/Library/Collections/ChecklistSpec.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace spec\Milhojas\Library\Collections;
+
+use Milhojas\Library\Collections\Checklist;
+use PhpSpec\ObjectBehavior;
+
+class ChecklistSpec extends ObjectBehavior
+{
+    private $elements = ['apple', 'banana', 'orange', 'pineapple'];
+
+    public function let()
+    {
+        $this->beConstructedWith($this->elements);
+    }
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(Checklist::class);
+    }
+
+    public function it_gets_all_elements_at_creation_time()
+    {
+        $this->asArray()->shouldBe($this->elements);
+    }
+
+    public function its_elements_are_not_checked_by_default()
+    {
+        foreach ($this->elements as $element) {
+            $this->shouldNotBeChecked($element);
+        }
+    }
+
+    public function it_can_check_one_item()
+    {
+        $this->check('banana');
+        $this->shouldBeChecked('banana');
+    }
+
+    public function it_can_uncheck_an_item()
+    {
+        $this->check('apple');
+        $this->uncheck('apple');
+        $this->shouldNotBeChecked('apple');
+    }
+
+    public function it_is_case_insensitive()
+    {
+        $this->check('APPLE');
+        $this->shouldBeChecked('apple');
+        $this->shouldBeChecked('AppLE');
+        $this->check('BanAnA');
+        $this->shouldBeChecked('banana');
+    }
+
+    public function it_can_say_that_two_lists_have_checked_items_in_common()
+    {
+        $list = clone $this;
+        $list->check('banana');
+        $this->check('banana');
+        $this->shouldHaveCoincidencesWith($list);
+    }
+
+    public function it_can_get_the_coincidences_between_two_lists()
+    {
+        $list = new Checklist($this->elements);
+        $list->check('banana');
+        $list->check('orange');
+        $this->check('banana');
+        $this->check('orange');
+        $this->check('apple');
+        $this->getCoincidencesWith($list)->shouldContain('banana');
+        $this->getCoincidencesWith($list)->shouldContain('orange');
+        $this->getCoincidencesWith($list)->shouldNotContain('apple');
+    }
+
+    public function it_can_not_speak_about_or_act_on_an_unsupported_item()
+    {
+        $this->shouldThrow('\InvalidArgumentException')->duringIsChecked('mango');
+        $this->shouldThrow('\InvalidArgumentException')->duringCheck('mango');
+        $this->shouldThrow('\InvalidArgumentException')->duringUnCheck('mango');
+    }
+}
---
+++ src/Milhojas/Library/Collections/Checklist.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Milhojas\Library\Collections;
+
+/**
+ * Represents an inmutable list of elements that can be checked/unchecked.
+ */
+class Checklist
+{
+    private $items;
+
+    /**
+     * @param mixed $elements The elements of the check list
+     */
+    public function __construct($elements)
+    {
+        $this->items = array_fill_keys($elements, false);
+    }
+
+    /**
+     * Returns the elements as an array.
+     *
+     * @return array The elements
+     */
+    public function asArray()
+    {
+        return array_keys($this->items);
+    }
+
+    /**
+     * Checks the element identified by $item.
+     *
+     * @param mixed $item
+     */
+    public function check($item)
+    {
+        $item = $this->normalize($item);
+        $this->isSupported($item);
+
+        $this->items[$item] = true;
+    }
+
+    /**
+     * Unchecks the element identified by $item.
+     *
+     * @param mixed $item
+     */
+    public function unCheck($item)
+    {
+        $item = $this->normalize($item);
+        $this->isSupported($item);
+
+        $this->items[$item] = false;
+    }
+
+    /**
+     * Tells if element $item is checked.
+     *
+     * @param mixed $item
+     *
+     * @return bool true if element is checked
+     */
+    public function isChecked($item)
+    {
+        $item = $this->normalize($item);
+        $this->isSupported($item);
+
+        return $this->items[$item];
+    }
+
+    public function hasCoincidencesWith($another_list)
+    {
+        return 0 !== count($this->getCoincidencesWith($another_list));
+    }
+
+    public function getCoincidencesWith($another_list)
+    {
+        $filter = function ($item) use ($another_list) {
+            return $this->items[$item] && $another_list->items[$item];
+        };
+
+        return array_filter($this->asArray(), $filter);
+    }
+
+    /**
+     * Normalizes input $item to ensure is lower case.
+     *
+     * @param mixed $item
+     *
+     * @return string normalized item
+     */
+    private function normalize($item)
+    {
+        return strtolower(trim($item));
+    }
+
+    /**
+     * Ensures element $item is managed by this list.
+     *
+     * @param mixed $item
+     *
+     * @throws \InvalidArgumentException if $item is not supported
+     */
+    private function isSupported($item)
+    {
+        if (!array_key_exists($item, $this->items)) {
+            throw new \InvalidArgumentException(sprintf('List doesn\'t know about "%s"', $item));
+        }
+    }
+}

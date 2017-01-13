70d17a88a2f14274248fc364dc420202deb080f1
franiglesias <franiglesias@mac.com>
2017-01-13 17:07:20 +0100

    Spec-ing EventRecorder and documenting EventBus

--- src/Milhojas/Library/Messaging/EventBus/Event.php
+++ src/Milhojas/Library/Messaging/EventBus/Event.php
@@ -2,8 +2,15 @@
 
 namespace Milhojas\Library\Messaging\EventBus;
 
-interface Event {
-	public function getName();
+/**
+ * Represents a declarative message about something that had happened in the app domain.
+ */
+interface Event
+{
+    /**
+     * The name that identifies this event.
+     *
+     * @return string a name context.event
+     */
+    public function getName();
 }
-
-?>
--- src/Milhojas/Library/Messaging/EventBus/EventRecorder.php
+++ src/Milhojas/Library/Messaging/EventBus/EventRecorder.php
@@ -2,77 +2,80 @@
 
 namespace Milhojas\Library\Messaging\EventBus;
 
-use Milhojas\Library\Messaging\EventBus\Event;
-
 /**
- * Records plain events to store them temporary and pass them later to an Event Dispatcher
+ * Records plain events to store them temporary and pass them later to an Event Dispatcher.
  *
- * @package milhojas.library.eventbus
  * @author Fran Iglesias
  */
-
 class EventRecorder implements \IteratorAggregate
 {
-	/**
-	 * Array of stored events
-	 */
-	private $events = array();
-	
-	public function getIterator()
-	{
-		return new \ArrayIterator($this->events);
-	}
-	
-	/**
-	 * Records an Event, appending it to the stored ones
-	 */
-	public function recordThat(Event $event)
-	{
-		$this->events[] = $event;
-	}
-	
-	/**
-	 * Records an array of events
-	 */
-	public function load(array $events)
-	{
-		foreach ($events as $event) {
-			$this->recordThat($event);
-		}
-	}
-	
-	/**
-	 * Retrieves a plain array of events
-	 */
-	public function retrieve()
-	{
-		return $this->events;
-	}
-	
-	/**
-	 * Empties the array of events. Use after currents events are dispatched
-	 */
-	public function flush()
-	{
-		$this->events = array();
-	}
-	
-	/**
-	 * Counts the events stored in the Recorder
-	 */	
-	public function count()
-	{
-		return count($this->events);
-	}
-	
-	public function __toString()
-	{
-		$string = '';
-		foreach ($this->events as $event) {
-			$string .= $event->getName().chr(10);
-		}
-		return $string;
-	}
-	
+    /**
+     * Array of stored events.
+     *
+     * @var array
+     */
+    private $events = array();
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->events);
+    }
+
+    /**
+     * Records an Event, appending it to the stored ones.
+     *
+     * @param Event $event the event to record
+     */
+    public function recordThat(Event $event)
+    {
+        $this->events[] = $event;
+    }
+
+    /**
+     * Records an array of events.
+     *
+     * @param array $events an array of events
+     */
+    public function load(array $events)
+    {
+        foreach ($events as $event) {
+            $this->recordThat($event);
+        }
+    }
+
+    /**
+     * Retrieves a plain array of events.
+     *
+     * @return array of events
+     */
+    public function retrieve()
+    {
+        return $this->events;
+    }
+
+    /**
+     * Empties the array of events. Use after current events are dispatched.
+     */
+    public function flush()
+    {
+        $this->events = array();
+    }
+
+    /**
+     * Counts the events stored in the Recorder.
+     */
+    public function count()
+    {
+        return count($this->events);
+    }
+
+    public function __toString()
+    {
+        $string = '';
+        foreach ($this->events as $event) {
+            $string .= $event->getName().chr(10);
+        }
+
+        return $string;
+    }
 }
-?>

ce52118e391b469a41ab721412901caf409205f3
franiglesias <franiglesias@mac.com>
2017-01-18 17:47:32 +0100

    Refix

--- src/Milhojas/Library/Messaging/EventBus/EventBus.php
+++ src/Milhojas/Library/Messaging/EventBus/EventBus.php
@@ -24,7 +24,7 @@ class EventBus
      */
     public function addListener($eventName, EventHandler $listener)
     {
-        $this->handlers[$eventName][] = $handler;
+        $this->handlers[$eventName][] = $listener;
     }
 
     /**

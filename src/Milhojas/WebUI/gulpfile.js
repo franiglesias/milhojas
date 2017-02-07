b666c399d8d03b551d527c9967b1d8a492913a36
franiglesias <franiglesias@mac.com>
2017-02-07 13:35:29 +0100

    Styling tables

--- src/Milhojas/WebUI/gulpfile.js
+++ src/Milhojas/WebUI/gulpfile.js
@@ -8,80 +8,63 @@ var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 // Paths
 var destination = '../../../web/assets';
-var sassSource = 'src/scss/**/*.scss';
 
+// Sources
+var sassSource = 'src/scss/**/*.scss';
 var jsVendorSource = 'src/js/vendor';
 var jsxSource = 'src/js/**/*.jsx';
 var jsSource = 'src/js/**/*.js';
+var htmlSource = 'src/**/*.html';
+
 // Tasks
 
-gulp.task('default', ['sass', 'copy-react', 'copy-react-dom', 'compile-jsx', 'scripts', 'watch']);
+gulp.task('default', [
+    'sass',
+    'copy-react',
+    'copy-react-dom',
+    'compile-jsx',
+    'scripts',
+    'html',
+    'watch'
+]);
 
 gulp.task('watch', function() {
     gulp.watch(jsxSource, ['copy-react', 'copy-react-dom', 'compile-jsx']);
     gulp.watch(jsSource, ['scripts']);
     gulp.watch(sassSource, ['sass']);
+    gulp.watch(htmlSource, ['html']);
 });
 
 // SASS compile to CSS
 gulp.task('sass', function() {
-    gulp.src(sassSource)
-        .pipe(sass({
-            style: 'expanded'
-        }))
-        .pipe(gulp.dest(destination + '/css'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(cssnano())
-        .pipe(gulp.dest(destination + '/css'))
+    gulp.src(sassSource).pipe(sass({style: 'expanded'})).pipe(gulp.dest(destination + '/css')).pipe(rename({suffix: '.min'})).pipe(cssnano()).pipe(gulp.dest(destination + '/css'))
 });
 
 // Javascript
 
-
 gulp.task('copy-react', function() {
-    return gulp.src('node_modules/react/dist/react.js')
-        .pipe(newer(jsVendorSource + '/react.js'))
-        .pipe(gulp.dest(jsVendorSource));
+    return gulp.src('node_modules/react/dist/react.js').pipe(newer(jsVendorSource + '/react.js')).pipe(gulp.dest(jsVendorSource));
 });
 gulp.task('copy-react-dom', function() {
-    return gulp.src('node_modules/react-dom/dist/react-dom.js')
-        .pipe(newer(jsVendorSource + '/react-dom.js'))
-        .pipe(gulp.dest(jsVendorSource));
+    return gulp.src('node_modules/react-dom/dist/react-dom.js').pipe(newer(jsVendorSource + '/react-dom.js')).pipe(gulp.dest(jsVendorSource));
 });
 
 gulp.task('copy-react-to-vendor', function() {
     return gulp.src([
-            jsVendorSource + '/react.js',
-            jsVendorSource + '/react-dom.js'
-        ])
-        .pipe(gulp.dest(destination + '/js/vendor'))
+        jsVendorSource + '/react.js',
+        jsVendorSource + '/react-dom.js'
+    ]).pipe(gulp.dest(destination + '/js/vendor'))
 
 });
 
 gulp.task('compile-jsx', () => {
-    return gulp.src(jsxSource)
-        .pipe(babel({
-            presets: ['react']
-        }))
-        .pipe(rename({
-            extname: '.js'
-        }))
-        .pipe(gulp.dest(destination + '/js'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest(destination + '/js'));
+    return gulp.src(jsxSource).pipe(babel({presets: ['react']})).pipe(rename({extname: '.js'})).pipe(gulp.dest(destination + '/js')).pipe(rename({suffix: '.min'})).pipe(uglify()).pipe(gulp.dest(destination + '/js'));
 });
 
 gulp.task('scripts', function() {
-    gulp.src(jsSource)
-        .pipe(gulp.dest(destination + '/js'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest(destination + '/js'));
+    gulp.src(jsSource).pipe(gulp.dest(destination + '/js')).pipe(rename({suffix: '.min'})).pipe(uglify()).pipe(gulp.dest(destination + '/js'));
 });
+
+gulp.task('html', function() {
+    gulp.src(htmlSource).pipe(gulp.dest(destination))
+})

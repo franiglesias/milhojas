afebdfe2560f0a44af6191d9fde1e0750b590d39
franiglesias <franiglesias@mac.com>
2017-02-16 09:34:48 +0100

    Removing inter context dependencies

--- src/Milhojas/Domain/Cantine/Specification/TicketSoldToStudent.php
+++ src/Milhojas/Domain/Cantine/Specification/TicketSoldToStudent.php
@@ -8,7 +8,7 @@ use Milhojas\Domain\Shared\StudentId;
 class TicketSoldToStudent extends TicketSpecification
 {
     private $studentId;
-    public function __construct(StudentId $id)
+    public function __construct($id)
     {
         $this->studentId = $id;
     }
